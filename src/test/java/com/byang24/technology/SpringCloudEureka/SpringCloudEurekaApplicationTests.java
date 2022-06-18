d773840ebc2f2f80acb84304d60d6f0df126ca83
Bryant Yang <chiru127@hotmail.com>
2022-06-18 00:40:45 -0700

    Initial Commit

---
+++ src/test/java/com/byang24/technology/SpringCloudEureka/SpringCloudEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byang24.technology.SpringCloudEureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
